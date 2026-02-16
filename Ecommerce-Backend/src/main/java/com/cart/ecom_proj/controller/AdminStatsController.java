6a1b5fa76b064b27427bbb27c23e043a8de884f5
Deepak Chilla <deepakchilla106@gmail.com>
2026-02-16 21:34:59 +0530

    fix: align Java version to 21, enable dynamic port binding, and clean CORS for deployment

--- Ecommerce-Backend/src/main/java/com/cart/ecom_proj/controller/AdminStatsController.java
+++ Ecommerce-Backend/src/main/java/com/cart/ecom_proj/controller/AdminStatsController.java
@@ -7,7 +7,6 @@ import com.cart.ecom_proj.repo.ProductRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,7 +18,6 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api/statistics")
-@CrossOrigin(origins = "http://localhost:5173", allowCredentials = "true")
 public class AdminStatsController {
 
     @Autowired
--- Ecommerce-Backend/src/main/java/com/cart/ecom_proj/controller/ProductController.java
+++ Ecommerce-Backend/src/main/java/com/cart/ecom_proj/controller/ProductController.java
@@ -13,7 +13,6 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequestMapping("/api")
 public class ProductController {
